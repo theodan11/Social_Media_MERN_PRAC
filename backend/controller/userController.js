0415c1986ad975aacec71b448199a43a1f050ec6
theodan11 <theophiljadango@gmail.com>
2025-03-12 18:45:55 +0600

    follow unfollow

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,6 +2,20 @@ import UserModel from '../model/userModel.js'
 import bcrypt from 'bcryptjs'
 
 
+
+const getUser = async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ _id: req.params.id })
+        const { password, ...other } = user._doc
+        return res.status(200).json(other)
+
+    } catch (error) {
+        return res.json(error)
+
+    }
+}
+
+
 const updateUser = async (req, res) => {
     try {
         if (req.body.password) {
@@ -20,5 +34,47 @@ const updateUser = async (req, res) => {
 
 }
 
-export { updateUser }
+
+const followUser = async (req, res) => {
+    try {
+        if (req.params.id !== req.body.id) {
+            const user = await UserModel.findById(req.body.id)
+            const followedUser = await UserModel.findById(req.params.id)
+            if (!followedUser.followers.includes(req.body.id)) {
+                await user.updateOne({ $push: { followings: req.params.id } })
+                await followedUser.updateOne({ $push: { followers: req.body.id } })
+                return res.status(200).json({ "msg": "followed" })
+            } else {
+                return res.status(403).json({ "msg": "illegal action" })
+    
+            }
+
+        }
+       
+    } catch (error) {
+        return res.status(200).json(error)
+    }
+}
+
+
+const unfollowUser = async (req, res) => {
+    try {
+        const user =await UserModel.findById(req.body.id)
+        const followingUser =await UserModel.findById(req.params.id)
+
+        if (followingUser.followers.includes(req.body.id)) {
+            await followingUser.updateOne({ $pull: { followers: req.body.id } })
+            await user.updateOne({ $pull: { followings: req.params.id } })
+            return res.status(200).json({ "msg": "unfollowed" })
+        }
+        else {
+            return res.status(403).json({ "msg": "illegal action" })
+
+        }
+    } catch (error) {
+        return res.status(403).json({"error":error})
+    }
+}
+
+export { updateUser, getUser, followUser, unfollowUser }
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyToken.js";
-import { updateUser } from "../controller/userController.js";
+import { updateUser, getUser, followUser, unfollowUser } from "../controller/userController.js";
 
 const router = express.Router()
 
 
+router.get('/:id', getUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', unfollowUser)
 router.put('/update/:id', verifyUser, updateUser)
-
 
 
 export default router
