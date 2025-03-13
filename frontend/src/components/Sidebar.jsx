9d7ff111d6f480bde0c24f3052b3795ed62abaab
theodan11 <theophiljadango@gmail.com>
2025-03-14 03:09:49 +0600

    sidebar com

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -47,8 +47,50 @@ const Sidebar = () => {
               <span className='sidebarListItemText'>See more</span>
             </li>
            <hr className='hrSidebar'/>
-            
+            <div className="yourShortcut">
+              <span >Your shortcuts</span>
+              <span className='editbtn'>Edit</span>
+            </div>
+
+            <li className='sidebarListItem'>
+              <img src="/assets/post/2.jpeg" className='sidebarShorcut' alt="" />
+              <span className='sidebarListItemText'>Jane Doe</span>
+            </li>
+            <li className='sidebarListItem'>
+              <img src="/assets/post/3.jpeg" className='sidebarShorcut' alt="" />
+              <span className='sidebarListItemText'>Jane Doe</span>
+            </li>
+            <li className='sidebarListItem'>
+              <img src="/assets/post/4.jpeg" className='sidebarShorcut' alt="" />
+              <span className='sidebarListItemText'>Jane Doe</span>
+            </li>
+            <li className='sidebarListItem'>
+              <img src="/assets/post/5.jpeg" className='sidebarShorcut' alt="" />
+              <span className='sidebarListItemText'>Jane Doe</span>
+            </li>
+
+            <li className='sidebarListItem '>
+              <KeyboardArrowDownTwoTone className='sidebarIcon seemore' />
+              <span className='sidebarListItemText'>See more</span>
+            </li>
           </ul>
+
+          <div className="linksList">
+            <p className='linkItem'>Privacy</p>
+            <p className=''> . </p>
+            <p className='linkItem'>Terms</p>
+            <p className=''> . </p>
+            <p className='linkItem'>Advertising</p>
+            <p className=''> . </p>
+            <p className='linkItem'>Ad Choices ;</p>
+            <p className=''> . </p>
+            <p className='linkItem'>Cookies</p>
+            <p className=''> . </p>
+            <p className='linkItem'>More</p>
+            <p className=''> . </p>
+            <p className='linkItem'>Theophil © 2025</p>
+
+          </div>
         </div>
     </div>
   )
